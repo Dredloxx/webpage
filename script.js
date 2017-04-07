a62b9190d6943e358e40020a24c31f642ecc9067
Isaiah Ramirez <Isaiah Ramirez>
2017-04-07 13:34:13 -0700

    Angular_Work

--- script.js
+++ script.js
@@ -2,12 +2,32 @@ angular.module('myApp',[])
 .controller('MainCtrl', function($scope) {
   $scope.bookmarks = [{
     label:'AngularJS',
-    url:'https://angularjs.org/'
+    url:'https://angularjs.org/',
+    catagory: 'Development'
   },{
     label:'Egghead.io',
-    url:'https://egghead.io/'
+    url:'https://egghead.io/',
+    catagory: 'Development'
   },{
-    label:'Node.js',
-    url:'https://nodejs.org/en/'
+    label:'Bootstrap',
+    url:'http://getbootstrap.com/',
+    catagory: 'Design'
+  },{
+    label:'One Page Love'
+    url:'https://onepagelove.com/'
+    catagory: 'Design'
+  },{
+    label:'MobilityWOD'
+    url:'http://www.mobilitywod.com/'
+    catagory: 'Exercise'
+  },{
+    label:'Robb Wolf'
+    url:'https://robbwolf.com/'
+    catagory: 'Exercise'
+  },{
+    label:'The Meta Picture'
+    url:'themetapicture.com'
+    catagory: 'Humor'
   }]
+
 });
